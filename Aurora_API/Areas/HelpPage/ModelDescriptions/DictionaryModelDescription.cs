6637968d222daf9391d0b3a0432d65a03495f7e7
haxuyhado <igorlecturer143@gmail.com>
2026-07-06 16:30:00 +0300

    Добавлен API-Проект + подключение к БД + установка библиотек

---
+++ Aurora_API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Aurora_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Aurora_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aurora_API.MvcApplication" Language="C#" %>
